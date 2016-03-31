3b16ac387cd4b270a7588411e4a5b92c121de319
MosNicholas <mosnicholas@berkeley.edu>
2016-03-31 09:19:59 -0700

    Separated audio & video in clipped videos

--- clip_vidoes.py
+++ clip_vidoes.py
@@ -5,8 +5,9 @@ import subprocess
 from moviepy.editor import VideoFileClip
 
 VIDEO_YOUTUBE_ID = 'RDrfE9I8_hs'
-RAW_VIDEO_FOLDER = os.path.join('video_data', 'raw')
-CLIPPED_VIDEO_FOLDER = os.path.join('video_data', 'clipped')
+DATA_FOLDER = 'data'
+RAW_VIDEO_FOLDER = os.path.join(DATA_FOLDER, 'raw')
+CLIPPED_VIDEO_FOLDER = os.path.join(DATA_FOLDER, 'clipped')
 
 TRUMP_FILENAME = 'china.mp4'
 TRUMP_OUTPUT_RAW_FILEPATH = os.path.join(RAW_VIDEO_FOLDER, 'trump', TRUMP_FILENAME)
@@ -23,14 +24,18 @@ def download_trump_video():
   return True
 
 def clip_trump_videos():
+  # To write video to file: clip.write_videofile(outfile, codec='libx264', audio_codec='aac', temp_audiofile='china-%02d.m4a' % i, remove_temp=True)
   cuts = [(3, 5), (6, 8), (9, 12)]
   video = VideoFileClip(TRUMP_OUTPUT_RAW_FILEPATH)
   subclips = [video.subclip(start, end) for (start, end) in cuts]
   for i in xrange(len(subclips)):
     clip = subclips[i]
-    outfile = os.path.join(TRUMP_OUTPUT_CLIPPED_FOLDER, 'china-%02d.mp4' % i)
-    clip.to_videofile(outfile, codec='libx264', audio_codec='aac', temp_audiofile='china-%02d.m4a' % i, remove_temp=True)
+    video_outfile = os.path.join(TRUMP_OUTPUT_CLIPPED_FOLDER, 'video', 'china-%02d.mp4' % i)
+    audio_outfile = os.path.join(TRUMP_OUTPUT_CLIPPED_FOLDER, 'audio', 'china-%02d.m4a' % i)
+    clip.write_videofile(video_outfile, codec='libx264', audio=False)
+    clip.audio.write_audiofile(audio_outfile, codec='aac')
   return True
 
 if __name__ == '__main__':
   clip_trump_videos()
+
