f81ca220897a7587ffe5d5d25d7be3302cafef2c
Brian <brian.a.pugh@gmail.com>
2016-04-28 02:12:47 -0700

    using nico's saving method and parallel

--- left_right_align/create_left_right_dataset.py
+++ left_right_align/create_left_right_dataset.py
@@ -5,6 +5,7 @@ import shutil
 import subprocess
 import numpy as np
 import moviepy
+import multiprocessing
 from scipy import misc
 from random import randint, random as rand
 from moviepy.editor import VideoFileClip, AudioFileClip, ImageSequenceClip, CompositeVideoClip, TextClip
@@ -27,6 +28,8 @@ parser.add_argument('--output_images', default=True,
   help='Output sequences of .jpeg files. If false, .mp4 videos will be generated.')
 parser.add_argument('--presentation_movie', default=False,
   help='Output sequences of .jpeg files. If false, .mp4 videos will be generated.')
+parser.add_argument('--frame_stride', default=False,
+  help='Output sequences will effectively reduce the frame rate by this factor.')
 args = parser.parse_args()
 
 #args.youtube_id = 'XIeFKTbg3Aw'
@@ -47,6 +50,8 @@ def main():
     download_youtube_video(args.youtube_id, args.youtube_target)
   if args.presentation_movie:
     split_video_pres()
+  elif args.frame_stride:
+    split_video_stride()
   else:
     split_video()
 
@@ -101,6 +106,92 @@ def split_video_pres():
   compositeMovie.write_videofile(os.path.join(args.target_folder, "pres_video.mp4"), codec='libx264', audio=False)
   return True
 
+def split_video_stride():
+  movie_title = os.path.split(args.source_path)[-1]
+  video = VideoFileClip(args.source_path, audio=False)
+  if (args.presentation_movie):
+    video = video.subclip((1, 8, 36), (1, 8, 41))
+  video = video.resize((128, 96))
+  if (args.presentation_movie):
+    video.write_videofile(os.path.join(args.target_folder, 'pres_video.mp4'), codec='libx264', audio=False)
+  framerate = video.fps
+  width = (np.size(video.get_frame(0), 1) - args.middle_gap_pixel_size) / 2
+  num_frames = int(video.fps * video.duration)
+  left_video = moviepy.video.fx.all.crop(video, x1=0, width=width)
+  right_video = moviepy.video.fx.all.crop(video, x1=width + args.middle_gap_pixel_size, width=width)
+  left_video = left_video.set_fps(1).set_duration(num_frames)
+  right_video = right_video.set_fps(1).set_duration(num_frames)
+
+  output_ind = args.output_starting_ind
+  file_prefix = "seg"
+  offsets = [0]*20000
+
+  print "Using stride 3..."
+  stride = 3
+  poss_left_frames = []
+  poss_right_frames = []
+
+  num_cpus = multiprocessing.cpu_count()
+  frames_per_process = 20000 / num_cpus
+  for i in xrange(num_cpus):
+    start_ind = i * frames_per_process
+    end_ind = num_frames if i == num_cpus - 1 else (i + 1)*frames_per_process
+    multiprocessing.Process(
+      target=process_inds_stride,
+      args=(left_video, right_video, start_ind, end_ind, offsets)
+    ).start()
+
+offsets_recorded = 0
+def record_offsets(start_ind, end_ind, offsets, offsets_to_add):
+  offsets[start_ind:end_ind] = offsets_to_add
+  offsets_recorded += 1
+  if offsets_recorded == multiprocessing.cpu_count():
+    with open(os.path.join(args.target_folder, 'offsets.csv'), 'w') as offset_csv_file:
+      w = csv.DictWriter(offset_csv_file, fieldnames=['id', 'offset_frames'])
+      w.writeheader()
+      w.writerows(offsets)
+
+def process_inds_stride(left_video, right_video, start_ind, end_ind, full_offsets):
+  file_prefix = 'seg'
+  offsets = []
+  output_ind = 1
+
+  for ind in xrange(start_ind, end_ind):
+
+    if ind % 4 == 0:
+      offset = randint(1,10)
+      offset_left = randint(0, 1) == 1 # coin flip
+
+      frames_out = range(ind, ind + 28)[::3]
+      offset_frames_out = range(ind + offset, ind + offset + 28)[::3]
+      #left_frames_out = poss_left_frames[0:28:3]
+      #offset_frames_out = poss_left_frames[offset:28+offset:3] if offset_left else poss_right_frames[offset:28+offset:3]
+
+      for arr_ind, frame_ind in enumerate(frames_out):
+        #misc.toimage(right_frame, cmin=np.min(right_frame), cmax=np.max(right_frame)).save(os.path.join(args.target_folder, (file_prefix + '-{:06d}-frame-{:02d}-right.jpeg').format(output_ind, frame_ind)))
+        right_video.save_frame(os.path.join(args.target_folder, (file_prefix + '-{:06d}-frame-{:02d}-right.jpeg').format(output_ind, arr_ind)), frame_ind)
+        left_video.save_frame(os.path.join(args.target_folder, (file_prefix + '-{:06d}-frame-{:02d}-left.jpeg').format(output_ind, arr_ind)), frame_ind)
+
+      offsets.append({ 'id': '%06d' % output_ind, 'offset_frames': 0 })
+      output_ind += 1
+
+      if (offset_left):
+        for arr_ind, frame_ind in enumerate(frames_out):
+          right_video.save_frame(os.path.join(args.target_folder, (file_prefix + '-{:06d}-frame-{:02d}-right.jpeg').format(output_ind, arr_ind)), frame_ind)
+        for arr_ind, frame_ind in enumerate(offset_frames_out):
+          left_video.save_frame(os.path.join(args.target_folder, (file_prefix + '-{:06d}-frame-{:02d}-left.jpeg').format(output_ind, arr_ind)), frame_ind)
+      else:
+        for arr_ind, frame_ind in enumerate(offset_frames_out):
+          right_video.save_frame(os.path.join(args.target_folder, (file_prefix + '-{:06d}-frame-{:02d}-right.jpeg').format(output_ind, arr_ind)), frame_ind)
+        for arr_ind, frame_ind in enumerate(frames_out):
+          left_video.save_frame(os.path.join(args.target_folder, (file_prefix + '-{:06d}-frame-{:02d}-left.jpeg').format(output_ind, arr_ind)), frame_ind)
+      output_ind += 1
+      offsets.append({ 'id': '{:06d}'.format(output_ind), 'offset_frames': offset })
+      if (ind % 40 == 0):
+        #print('Finished processing {:d} outputs.'.format(output_ind-1))
+        print('At video frame ' + str(ind) + ' of ' + str(20000))
+  record_offsets(start_ind, end_ind, full_offsets, offsets)
+  return True
 
 def split_video():
 
@@ -123,6 +214,8 @@ def split_video():
   offsets = []
 
   for ind, left_frame in enumerate(left_video.iter_frames()):
+    if ind > 800:
+      break
     left_frame = rgb2gray(left_frame)
     right_frame = rgb2gray(right_frame_iterator.next())
     if (ind % 20 == 0): # INITIALIZE
@@ -160,7 +253,7 @@ def split_video():
       if (offset_left):
         if args.output_images:
           for frame_ind, offset_frame in enumerate(offset_frames):
-            misc.toimage(offset_frame, cmin=np.min(left_frame), cmax=np.max(left_frame)).save(os.path.join(args.target_folder, (file_prefix + '-{:06d}-frame-{:02d}-left.jpeg').format(output_ind, frame_ind)))
+            misc.toimage(offset_frame, cmin=np.min(offset_frame), cmax=np.max(offset_frame)).save(os.path.join(args.target_folder, (file_prefix + '-{:06d}-frame-{:02d}-left.jpeg').format(output_ind, frame_ind)))
           for frame_ind, right_frame in enumerate(right_frames):
             misc.toimage(right_frame, cmin=np.min(right_frame), cmax=np.max(right_frame)).save(os.path.join(args.target_folder, (file_prefix + '-{:06d}-frame-{:02d}-right.jpeg').format(output_ind, frame_ind)))
         else:
@@ -173,7 +266,7 @@ def split_video():
           for frame_ind, left_frame in enumerate(left_frames):
             misc.toimage(left_frame, cmin=np.min(left_frame), cmax=np.max(left_frame)).save(os.path.join(args.target_folder, (file_prefix + '-{:06d}-frame-{:02d}-left.jpeg').format(output_ind, frame_ind)))
           for frame_ind, offset_frame in enumerate(offset_frames):
-            misc.toimage(offset_frame, cmin=np.min(right_frame), cmax=np.max(right_frame)).save(os.path.join(args.target_folder, (file_prefix + '-{:06d}-frame-{:02d}-right.jpeg').format(output_ind, frame_ind)))
+            misc.toimage(offset_frame, cmin=np.min(offset_frame), cmax=np.max(offset_frame)).save(os.path.join(args.target_folder, (file_prefix + '-{:06d}-frame-{:02d}-right.jpeg').format(output_ind, frame_ind)))
         else:
           left_video_out = ImageSequenceClip(left_frames, fps=framerate)
           left_video_out.write_videofile(os.path.join(args.target_folder, file_prefix + '-{:06d}-left.mp4' % output_ind), codec='libx264', audio=False)
@@ -181,7 +274,7 @@ def split_video():
           right_video_out.write_videofile(os.path.join(args.target_folder, file_prefix + '-{:06d}-right.mp4' % output_ind), codec='libx264', audio=False)
       offsets.append({ 'id': '{:06d}'.format(output_ind), 'offset_frames': offset })
       output_ind += 1
-    if (ind % 1000 == 0):
+    if (ind % 100 == 0):
       print('Finished processing {:d} datapoints.'.format(output_ind))
   with open(offset_csv, 'w') as offset_csv_file:
     w = csv.DictWriter(offset_csv_file, fieldnames=['id', 'offset_frames'])
