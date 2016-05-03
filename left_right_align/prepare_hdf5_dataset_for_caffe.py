757f749cd8f6535813040bc7d103d59f2b174564
Brian <brian.a.pugh@gmail.com>
2016-05-03 09:25:36 -0700

    raise lr, create bin labels in h5 preparation

--- left_right_align/prepare_hdf5_dataset_for_caffe.py
+++ left_right_align/prepare_hdf5_dataset_for_caffe.py
@@ -265,6 +265,9 @@ def main():
 				label_mat = np.zeros((1, 1, 1, 1))
 				label_mat[0, 0, 0, 0] = offsets[seg1_ind]
 				f['label'] = label_mat
+				label_mat = np.zeros((1, 1, 1, 1))
+				label_mat[0, 0, 0, 0] = offsets_bin[seg1_ind]
+				f['label_bin'] = label_mat
 				#print("Writing to " + h5_location1)
 
 			if in_train[seg1_ind]:
@@ -282,6 +285,9 @@ def main():
 				label_mat = np.zeros((1, 1, 1, 1))
 				label_mat[0, 0, 0, 0] = offsets[seg2_ind]
 				f['label'] = label_mat
+				label_mat = np.zeros((1, 1, 1, 1))
+				label_mat[0, 0, 0, 0] = offsets_bin[seg2_ind]
+				f['label_bin'] = label_mat
 
 			if in_train[seg2_ind]:
 				filenames_train.append(h5_location2)
