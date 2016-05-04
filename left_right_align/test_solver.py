94e8f4e29e26cd024d0ef228b58ef90efcf54ab2
Brian <brian.a.pugh@gmail.com>
2016-05-04 11:04:13 -0700

    test for solver

--- left_right_align/test_solver.py
+++ left_right_align/test_solver.py
@@ -2,6 +2,7 @@ import numpy as np
 import caffe
 import os
 import argparse
+from matplotlib import pyplot as plt
 
 
 parser = argparse.ArgumentParser(
@@ -20,7 +21,7 @@ def test2_solver():
 
 	asc = np.tile(np.linspace(0, 1, num=10).reshape((1, 1, 10, 1, 1)), (1, 1, 1, 96, 64))
 
-	for _ in range(20):
+	for _ in range(1):
 		min_frame1 = np.random.randint(255)
 		max_frame1 = np.random.randint(min_frame1 + 1, 256)
 		range1 = max_frame1 - min_frame1
@@ -44,6 +45,16 @@ def test2_solver():
 		print("Left is {:.3f} to {:.3f}. Right is {:.3f} to {:.3f}." \
 			.format(lesser[0], lesser[0] + lesser[1], greater[0], greater[0] + greater[1]))
 
+		plt.close('all')
+		fig = plt.figure()
+		for frame_ind in range(0, 10):
+			sub = fig.add_subplot(2, 11, frame_ind)
+			plt.imshow(stacked_left1[0, 0, frame_ind, :, :])
+			sub.set_title('Frame {:d}'.format(frame_ind+1))
+			sub = fig.add_subplot(2, 11, 11 + plot_ind)
+			plt.imshow(stacked_right1[0, 0, frame_ind, :, :])
+			sub.set_title('Frame {:d}'.format(frame_ind+1))
+
 		stacked_left1 *= 1.0/255
 		stacked_right1 *= 1.0/255
 		stacked_left2 *= 1.0/255
@@ -55,7 +66,13 @@ def test2_solver():
 		#print(stacked_right1)
 		net.forward()
 		result = net.blobs['activations'].data
+		sub = fig.add_subplot(2, 11, 11)
+		plt.bar(1, result[0], .35, color='r')
+		plt.bar(1.35, result[1], .35, color='y')
+		sub.set_ylabel('Activations')
+		sub.set_xticklabels(('left faster', 'right faster'))
 		print("Expected {:d}, got {:s}.".format(0, result))
+		plt.show()
 
 		net.blobs['left'].data[...] = stacked_left2
 		#print(stacked_left1)
