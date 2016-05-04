51c45efda98de46e39b02c73b9cf5bae42ee73c5
Brian <brian.a.pugh@gmail.com>
2016-05-04 10:24:08 -0700

    test for solver

--- left_right_align/test_solver.py
+++ left_right_align/test_solver.py
@@ -20,7 +20,7 @@ def test2_solver():
 
 	asc = np.tile(np.linspace(0, 1, num=10).reshape((1, 1, 10, 1, 1)), (1, 1, 1, 96, 64))
 
-	for _ in range(1):
+	for _ in range(20):
 		min_frame1 = np.random.randint(255)
 		max_frame1 = np.random.randint(min_frame1 + 1, 256)
 		range1 = max_frame1 - min_frame1
@@ -36,9 +36,13 @@ def test2_solver():
 
 		stacked_left1 = asc * greater[1] + greater[0]
 		stacked_right1 = asc * lesser[1] + lesser[0]
+		print("Left is {:.3f} to {:.3f}. Right is {:.3f} to {:.3f}." \
+			.format(greater[0], greater[0] + greater[1], lesser[0], lesser[0] + lesser[1]))
 
 		stacked_left2 = asc * lesser[1] + lesser[0]
 		stacked_right2 = asc * greater[1] + greater[0]
+		print("Left is {:.3f} to {:.3f}. Right is {:.3f} to {:.3f}." \
+			.format(lesser[0], lesser[0] + lesser[1], greater[0], greater[0] + greater[1]))
 
 		stacked_left1 *= 1.0/255
 		stacked_right1 *= 1.0/255
@@ -46,19 +50,19 @@ def test2_solver():
 		stacked_right2 *= 1.0/255
 
 		net.blobs['left'].data[...] = stacked_left1
-		print(stacked_left1)
+		#print(stacked_left1)
 		net.blobs['right'].data[...] = stacked_right1
-		print(stacked_right1)
+		#print(stacked_right1)
 		net.forward()
-		result = net.blobs['fc8'].data
+		result = net.blobs['out'].data
 		print("Expected {:d}, got {:s}.".format(0, result))
 
 		net.blobs['left'].data[...] = stacked_left2
-		print(stacked_left1)
+		#print(stacked_left1)
 		net.blobs['right'].data[...] = stacked_right2
-		print(stacked_right1)
+		#print(stacked_right1)
 		net.forward()
-		result = net.blobs['fc8'].data
+		result = net.blobs['out'].data
 		print("Expected {:d}, got {:s}.".format(1, result))
 
 test2_solver()
